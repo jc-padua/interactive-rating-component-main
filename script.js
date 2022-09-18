c32e7d80e96bcd900cf8a9141b82afdd5921f01f
jrkpadua <jcp093001@gmail.com>
2022-09-19 06:36:19 +0800

    Animations added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const rateNum = document.querySelectorAll('.rate-num');
 const submit = document.querySelector('.btn');
-const thankyouState = document.querySelector('.thankyou-state')
-const rateText = document.querySelector('.rate-selected')
+const thankyouState = document.querySelector('.thankyou-state');
+const rateText = document.querySelector('.rate-selected');
 
-function displaySelected() {
+const rateContainer = document.querySelector('.rate-container');
+
+function rateSelected() {
     rateNum.forEach(num => {
         num.classList.remove('selected')
     })
@@ -17,11 +19,25 @@ function displayThankyou() {
             rateText.textContent = rateText.textContent.replace('0', rateValue); //Display the value to thankyou state
             thankyouState.style.zIndex = '1';                                    //Then Display the thank you state
             thankyouState.style.visibility = 'visible';
+            document.querySelector('.thankyou-icon').classList.add('show');
+            rateText.classList.add('show');
+        } else {
+            rateContainer.classList.add('shakeItOff')
         }
     })
 }
+// function rateRequire() {
+//     console.log('Shake Start');
+// }
+
+function removeShake() {
+    rateContainer.classList.remove('shakeItOff');
+    // console.log('Shake Removed');
+}
 
 
-rateNum.forEach(num => { num.addEventListener('click', displaySelected) })
-submit.addEventListener('click', displayThankyou)
+rateNum.forEach(num => { num.addEventListener('click', rateSelected) });
+submit.addEventListener('click', displayThankyou);
+// rateContainer.addEventListener('animationstart', rateRequire); Trial and Error. For Checking if animations fired
+rateContainer.addEventListener('animationend', removeShake);
 
